52362eb70387f3ef5dbb02d3be5c306f2fee07ab
upkero <upkero@users.noreply.github.com>
2025-04-28 16:25:25 +0300

    Create main test version

---
+++ XShop/main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from main import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('delivery/', views.delivery, name='delivery'),
+    path('policy/', views.policy, name='policy'),
+    path('terms/', views.terms, name='terms'),
+    path('order-and-return/', views.ordandret, name='order_and_return'),
+]
--- XShop/main/views.py
+++ XShop/main/views.py
@@ -1,2 +1,40 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import context
+
+
+def index(request):
+    context = {
+        'products':[
+            {'image': 'img/goods/1.png', 'name': 'Green Tea', 'strength': 2, 'price': 15.00},
+            {'image': 'img/goods/2.png', 'name': 'Black Tea', 'strength': 9, 'price': 10.00},
+            {'image': 'img/goods/3.png', 'name': 'White Tea', 'strength': 5, 'price': 20.00},
+            {'image': 'img/goods/4.png', 'name': 'Jasmine Tea', 'strength': 6, 'price': 56.00},
+        ]
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+
+def delivery(request):
+    return render(request, 'main/delivery.html')
+
+
+def policy(request):
+    return render(request, 'main/policy.html')
+
+
+def terms(request):
+    return render(request, 'main/terms.html')
+
+
+def ordandret(request):
+    return render(request, 'main/ordandret.html')
+
