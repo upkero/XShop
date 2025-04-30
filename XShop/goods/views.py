f2feda9ba5ec4de79afb397295de3d93471829c0
upkero <upkero@users.noreply.github.com>
2025-04-30 20:40:44 +0300

    Add products url

--- XShop/goods/views.py
+++ XShop/goods/views.py
@@ -14,8 +14,11 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
+def product(request, product_id):
+    product = Products.objects.get(id=product_id)
+    
     context = {
-        'product': Products.objects.filter(id=11)[0]
+        'product' : product,
     }
+    
     return render(request, 'goods/product.html', context)
