4287c29ab9dc73fef356e5fcc00ca08e582d97b5
upkero <upkero@users.noreply.github.com>
2025-05-01 18:01:10 +0300

    Add logic for for sorting on the page

--- XShop/goods/views.py
+++ XShop/goods/views.py
@@ -7,14 +7,25 @@ from goods.models import Categories, Products
 def catalog(request, category_slug=False):
     
     page = request.GET.get('page', 1)
+    
+    
+    order_by = request.GET.get('order_by', 'default')
+    
+    
     on_sale = request.GET.get('on_sale', None)
     new = request.GET.get('new', None)
     # favorites = request.GET.get('favorites', None)
+    
     
     if category_slug:
         products = get_list_or_404(Products.objects.filter(is_active=True, category__slug=category_slug))
     else:
         products = Products.objects.filter(is_active=True)
+    
+    
+    if order_by and order_by != 'default':
+        products = products.order_by(order_by)
+    
     
     if on_sale:
         products = products.filter(discount__gt=0)
@@ -23,16 +34,20 @@ def catalog(request, category_slug=False):
     # if favorites:
     #     products = products.filter()
     
+    
     paginator = Paginator(products, 4)
     current_page = paginator.page(int(page))
+    
     
     context = {
         'products': current_page,
         'slug_url': category_slug,
     }
     
+    
     if category_slug:
         context['category'] = Categories.objects.get(slug = category_slug)
+    
     
     return render(request, 'goods/catalog.html', context)
 
