52071c4410bba4a6284e470bf37a2b80e9d40501
upkero <upkero@users.noreply.github.com>
2025-05-01 23:45:59 +0300

    Move base.html to global templates

--- XShop/core/settings.py
+++ XShop/core/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'temlates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
