c9dc6ed223367a2a646a704a1fc63f9d97b484ca
upkero <upkero@users.noreply.github.com>
2025-05-01 22:55:50 +0300

    Upgrade full text search

--- XShop/goods/utils.py
+++ XShop/goods/utils.py
@@ -1,4 +1,6 @@
-from django.db.models import Q
+from winreg import QueryReflectionKey
+from django.db.models import Q, Value
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector, SearchHeadline
 
 from goods.models import Products
 
@@ -8,12 +10,39 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description")
+    search_query = SearchQuery(query)
     
-    q_objects = Q()
-    
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    word_count = len(query.strip().split())
+    if word_count <= 2:
+        normalization = Value(2)
+    else:
+        normalization = Value(2).bitor(Value(4))
         
-    return Products.objects.filter(q_objects)
+    result = (
+        Products.objects
+        .annotate(rank=SearchRank(vector, search_query, normalization=normalization))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+    
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: rgba(201, 55, 137, 0.3);">',
+            stop_sel='</span>',
+        )
+    )
+    
+    return result
+    
+    # keywords = [word for word in query.split() if len(word) > 2]
+    
+    # q_objects = Q()
+    
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+        
+    # return Products.objects.filter(q_objects)
--- XShop/goods/views.py
+++ XShop/goods/views.py
@@ -6,62 +6,53 @@ from goods.utils import q_search
 
 
 def catalog(request, category_slug=False):
-    
-    page = request.GET.get('page', 1)
-    
-    
-    query = request.GET.get('q', None)
-    
-    
-    order_by = request.GET.get('order_by', 'default')
-    
-    on_sale = request.GET.get('on_sale', None)
-    new = request.GET.get('new', None)
+
+    page = request.GET.get("page", 1)
+
+    query = request.GET.get("q", None)
+
+    order_by = request.GET.get("order_by", "default")
+
+    on_sale = request.GET.get("on_sale", None)
+    new = request.GET.get("new", None)
     # favorites = request.GET.get('favorites', None)
-    
-    
+
     if query:
         products = q_search(query)
     elif not category_slug:
         products = Products.objects.filter(is_active=True)
     else:
         products = Products.objects.filter(is_active=True, category__slug=category_slug)
-    
-    
-    if order_by and order_by != 'default':
+
+    if order_by and order_by != "default":
         products = products.order_by(order_by)
-    
-    
+
     if on_sale:
         products = products.filter(discount__gt=0)
     if new:
         products = products.filter(is_new=True)
     # if favorites:
     #     products = products.filter()
-    
-    
+
     paginator = Paginator(products, 4)
     current_page = paginator.page(int(page))
-    
-    
+
     context = {
-        'products': current_page,
-        'slug_url': category_slug,
+        "products": current_page,
+        "slug_url": category_slug,
     }
-    
-    
+
     if category_slug:
-        context['category'] = Categories.objects.get(slug = category_slug)
-    
-    
-    return render(request, 'goods/catalog.html', context)
+        context["category"] = Categories.objects.get(slug=category_slug)
+
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request, product_slug):
     product = Products.objects.filter(slug=product_slug, is_active=True).first()
-    
+
     context = {
-        'product' : product,
+        "product": product,
     }
-    
-    return render(request, 'goods/product.html', context)
+
+    return render(request, "goods/product.html", context)
