f5b9ef65fe0e0e45facf7e23715c7a2857b5772c
upkero <upkero@users.noreply.github.com>
2025-05-02 00:25:17 +0300

    Fix TemplateDoesNotExist

--- XShop/core/settings.py
+++ XShop/core/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'temlates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
