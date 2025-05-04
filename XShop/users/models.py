6f9d5866de7a206c68b30b099c67299b5eda0559
upkero <upkero@users.noreply.github.com>
2025-05-04 22:54:21 +0300

    Add registration and restrictions for not authenticated users

--- XShop/users/models.py
+++ XShop/users/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
+    email = models.EmailField(
+        _("email address"), 
+        unique=True,
+        blank=True,
+        error_messages={
+            "unique": _("This email is already in use."),
+        },
+    )
     image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name="Profile picture")
     
     class Meta:
--- XShop/users/views.py
+++ XShop/users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -26,8 +26,18 @@ def login(request):
 
 
 def registration(request):
-    context = {
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
         
+    context = {
+        'form': form
     }
     return render(request, 'users/registr.html', context)
 
@@ -40,7 +50,8 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
 
 
 def changepass(request):
