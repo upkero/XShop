b3dba8f4855495a44eb3f26369face4edac49ea5
upkero <upkero@users.noreply.github.com>
2025-05-05 23:12:45 +0300

    Add cart remove function

--- XShop/carts/urls.py
+++ XShop/carts/urls.py
@@ -7,5 +7,5 @@ app_name = 'carts'
 urlpatterns = [
     path('add/<slug:product_slug>/', views.cart_add, name='add'),
     path('change/<slug:product_slug>/', views.cart_change, name='change'),
-    path('remove/<slug:product_slug>/', views.cart_remove, name='remove'),
+    path('remove/<int:cart_id>/', views.cart_remove, name='remove'),
 ]
