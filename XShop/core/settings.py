7771c7638e7281ab00df721be4b80bb724461fc1
upkero <upkero@users.noreply.github.com>
2025-05-05 16:27:53 +0300

    Add redirect unauthenticated users to login page

--- XShop/core/settings.py
+++ XShop/core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import LOGIN_URL
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,3 +153,5 @@ INTERNAL_IPS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_URL = '/user/login/'
