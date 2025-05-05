d5e0100d338621dbafc65757b2ed1859d4e7c3d1
upkero <upkero@users.noreply.github.com>
2025-05-05 21:24:34 +0300

    Create cart model

--- XShop/carts/views.py
+++ XShop/carts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 
-def cart_add(request, product_id):
+def cart_add(request, product_slug):
     ...
 
 
-def cart_change(request, product_id):
+def cart_change(request, product_slug):
     ...
     
     
-def cart_remove(request, product_id):
+def cart_remove(request, product_slug):
     ...
