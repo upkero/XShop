9e09a47118bc5350b4fabc7f68ef6babb990e2d2
upkero <upkero@users.noreply.github.com>
2025-05-07 22:23:44 +0300

    Setup order admin panel

--- XShop/orders/admin.py
+++ XShop/orders/admin.py
@@ -2,6 +2,77 @@ from django.contrib import admin
 
 from orders.models import Order, OrderItem
 
-# Register your models here.
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+class OrderItemTabulareAdmin(admin.TabularInline):
+    model = OrderItem
+    fields = 'product', 'name', 'price', 'quantity'
+    search_fields = [
+        'product',
+        'name',
+    ]
+    extra = 0
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = 'order', 'product', 'name', 'price', 'quantity'
+    search_fields = [
+        'order',
+        'product',
+        'name',
+    ]
+
+class OrderTabularAdmin(admin.TabularInline):
+    model = Order
+    fields = [
+        'requires_delivery', 
+        'status', 
+        'payment_on_get', 
+        'is_paid', 
+        'created_timestamp'
+    ]
+    search_fields = [
+        'id',
+        'requires_delivery',
+        'payment_on_get',
+        'is_paid',
+        'created_timestamp',
+    ]
+    readonly_fields = ['created_timestamp',]
+    extra = 0
+    
+    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    
+    fields = [
+        ('user', 'phone_number'),
+        'requires_delivery',
+        ('delivery_address_city', 'delivery_address_street', 'delivery_address_house', 'delivery_address_apartment'),
+        'delivery_notes',
+        'status',
+        'payment_on_get',
+        'is_paid',
+        'created_timestamp',
+    ]
+    
+    list_display = [
+        'id',
+        'user',
+        'requires_delivery',
+        'status',
+        'payment_on_get',
+        'is_paid',
+        'created_timestamp',
+    ]
+    
+    search_fields = [
+        'id',
+    ]
+    readonly_fields = ['created_timestamp',]
+    list_filter = [
+        'requires_delivery',
+        'status',
+        'payment_on_get',
+        'is_paid',
+    ]
+    inlines = (OrderItemTabulareAdmin,)
--- XShop/users/admin.py
+++ XShop/users/admin.py
@@ -1,7 +1,7 @@
-import profile
 from django.contrib import admin
 
 from carts.admin import CartTabAdmin
+from orders.admin import OrderTabularAdmin
 from users.models import User
 
 @admin.register(User)
@@ -9,7 +9,7 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'first_name', 'last_name', 'email']
     search_fields = ['first_name', 'last_name', 'username', 'email']
     
-    inlines = [CartTabAdmin,]
+    inlines = [CartTabAdmin, OrderTabularAdmin]
     
     fields = [
         'username', 
