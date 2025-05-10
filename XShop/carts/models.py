c98ea3c372ee4b2c2b9d521322149b78b04881ea
upkero <upkero@users.noreply.github.com>
2025-05-10 18:03:41 +0300

    Add ordering for carts

--- XShop/carts/models.py
+++ XShop/carts/models.py
@@ -27,6 +27,7 @@ class Cart(models.Model):
         db_table = 'cart'
         verbose_name = 'Cart'
         verbose_name_plural = 'Carts'
+        ordering = ('id',)
         
     objects = CartQueryset().as_manager()
         
