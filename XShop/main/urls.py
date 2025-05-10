be2a29f2cb6e73ed8dc9da710c323ae9246ac65a
upkero <upkero@users.noreply.github.com>
2025-05-11 00:35:52 +0300

    Switch main:index to CBV

--- XShop/main/urls.py
+++ XShop/main/urls.py
@@ -5,7 +5,7 @@ from main import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('delivery/', views.delivery, name='delivery'),
--- XShop/main/views.py
+++ XShop/main/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Products
 import random
 
 
-def index(request):
-
-    products = list(Products.objects.filter(is_active=True, category__is_active=True))
-    random.shuffle(products)
-    recommended_products = products[:4]
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
     
-    context = {
-        'recommended_products': recommended_products,
-    }
-    return render(request, 'main/index.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        products = list(Products.objects.filter(is_active=True, category__is_active=True))
+        random.shuffle(products)
+        
+        context['recommended_products'] = products[:4]
+        
+        return context
 
 
 def about(request):
