2a53194aa82d1c8788842ad4b14ccf4388e21d35
upkero <upkero@users.noreply.github.com>
2025-05-27 21:50:54 +0300

    Add reviews model

--- XShop/reviews/models.py
+++ XShop/reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from goods.models import Products
+
+
+class ReviewModel(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='User')
+    product = models.ForeignKey(to=Products, on_delete=models.CASCADE, verbose_name='Product')
+    rating = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 6)], verbose_name='Rating')
+    comment = models.TextField(verbose_name='Comment')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+    
+    class Meta:
+        db_table = 'review'
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
+        ordering = ('-created_at',)
+        unique_together = ('user', 'product')
+        
+    def __str__(self):
+        return f"{self.user.username} on {self.product.name} ({self.rating}★)"
+    
